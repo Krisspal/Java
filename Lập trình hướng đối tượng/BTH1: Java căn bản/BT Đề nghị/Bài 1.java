2649483a72d352721a3cdd3e9d47813852a83f45
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-10-08 10:41:58 +0700

    Update Bài 1.java

--- Lập trình hướng đối tượng/BTH1: Java căn bản/BT Đề nghị/Bài 1.java
+++ Lập trình hướng đối tượng/BTH1: Java căn bản/BT Đề nghị/Bài 1.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class Main{
+	public static void main(String []args) {
+		int n, count = 0;
+		int result = (int ) (Math.random() * 100 + 1);
+		do {
+			count ++;
+			System.out.print("Nhap dap an cua ban (1 - 100): ");
+			Scanner input = new Scanner(System.in);
+			n = input.nextInt();
+			if (n > result) {
+				System.out.println("Dap an cua ban lon hon ket qua");
+			}
+			else if (n < result) {
+				System.out.println("Dap an cua ban nho hon ket qua");
+			}
+			else {
+				System.out.println("Ban da tra loi dung!");
+				break;
+				}
+			}while (n != result);
+		System.out.println("So lan doan: " + count + " lan");
+	}
+}
